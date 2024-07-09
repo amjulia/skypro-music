3452487b3346ed0ed97379df39238c46f4fb426c
Karpova Julia <am_julia@mail.ru>
2024-07-09 16:21:17 +0600

    поиск работает

--- src/app/tracks/favorite/page.tsx
+++ src/app/tracks/favorite/page.tsx
@@ -1,17 +1,20 @@
 "use client";
 import { Centerblock } from "@/components/Centerblock/Centerblock";
 import { useAppDispatch, useAppSelector } from "@/hooks/store";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import { setPlaylist } from "@/store/features/playlistSlice";
 import Link from "next/link";
 
 export default function FavoriteTrackPage() {
   const dispatch = useAppDispatch();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const logged = useAppSelector((state) => state.auth.tokens.access);
   const tracks = useAppSelector((state) => state.playlist.likedTracks);
+  const filterTracks = useAppSelector((state) => state.playlist.filterPlaylist);
   useEffect(() => {
     dispatch(setPlaylist({ tracks: tracks })), console.log(tracks);
+    setIsLoading(false);
   }, [dispatch, tracks]);
 
   return (
@@ -19,7 +22,12 @@ export default function FavoriteTrackPage() {
       {logged ? (
         <>
           <h2 className={styles.centerblockH2}>Мой плейлист</h2>{" "}
-          <Centerblock tracks={tracks} />{" "}
+          <Centerblock tracks={filterTracks} />{" "}
+          {isLoading
+          ? "Загрузка"
+          : filterTracks.length === 0
+          ? "Треки не найдены"
+          : null}
         </>
       ) : (
         <Link href={"/signin"} className={styles.link}>
